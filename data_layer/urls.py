4b70a83e684c3e90545ec4823e38e084a9705203
Mark Stacy <mbstacy@gmail.com>
2014-06-12 11:04:20 -0500

    Update to urls for rest app

--- data_layer/urls.py
+++ data_layer/urls.py
@@ -3,10 +3,16 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from rest_framework import routers
+from rest_app.views import DokarrsViewSet
+router = routers.DefaultRouter()
+router.register('dokarrs',DokarrsViewSet)
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'data_layer.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', include(admin.site.urls)),
 )
