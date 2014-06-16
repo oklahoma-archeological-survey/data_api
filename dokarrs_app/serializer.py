7ea7d2bfe3c4ce65ffc5313e90e912f18f757b26
Mark Stacy <mbstacy@gmail.com>
2014-06-16 11:28:21 -0500

    fields kwargs added to customize the fields returned

--- dokarrs_app/serializer.py
+++ dokarrs_app/serializer.py
@@ -4,7 +4,26 @@ from models import Dokarrs
 import django_filters
 
 
-class DokarrsSerializer(serializers.HyperlinkedModelSerializer):
+class DynamicFieldsModelSerializer(serializers.ModelSerializer):
+    """
+    A ModelSerializer that takes an additional `fields` argument that
+    controls which fields should be displayed.
+    """
+
+    def __init__(self, *args, **kwargs):
+        # Instantiate the superclass normally
+        super(DynamicFieldsModelSerializer, self).__init__(*args, **kwargs)
+
+        fields = self.context['request'].QUERY_PARAMS.get('fields')
+        if fields:
+            fields = fields.split(',')
+            # Drop any fields that are not specified in the `fields` argument.
+            allowed = set(fields)
+            existing = set(self.fields.keys())
+            for field_name in existing - allowed:
+                self.fields.pop(field_name)
+
+class DokarrsSerializer(DynamicFieldsModelSerializer, serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Dokarrs
@@ -29,9 +48,12 @@ class DokarrsFilter(django_filters.FilterSet):
     citatin = django_filters.CharFilter(lookup_type='icontains')
     family = django_filters.CharFilter(lookup_type='icontains')
     obsnum = django_filters.CharFilter(lookup_type='icontains')
+    min_date = django_filters.DateFilter(name='timestamp', lookup_type='gte')
+    max_date = django_filters.DateFilter(name='timestamp', lookup_type='lte')
 
     class Meta:
         model = Dokarrs
         fields = ['genus', 'species', 'common_name', 'county', 'local', 'obsvr',
                   'month', 'year', 'class_field', 'day', 'remarks', 'citatin', 'family',
                   'obsnum']
+
