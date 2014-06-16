3b87da0d2ec3d1f02f0ac961d0a6a2122e56b8b8
Mark Stacy <mbstacy@gmail.com>
2014-06-16 12:09:36 -0500

    Updated search fields and removed renderer class for view and placed in settings.py

--- dokarrs_app/views.py
+++ dokarrs_app/views.py
@@ -5,15 +5,15 @@ from rest_framework import viewsets, filters
 
 from models import Dokarrs
 from serializer import DokarrsSerializer, DokarrsFilter
-from rest_framework.renderers import JSONRenderer, YAMLRenderer, JSONPRenderer,BrowsableAPIRenderer
+from rest_framework.renderers import JSONRenderer, YAMLRenderer, JSONPRenderer, BrowsableAPIRenderer,XMLRenderer
+
 
 class DokarrsViewSet(viewsets.ModelViewSet):
-
-    renderer_classes = (BrowsableAPIRenderer, JSONRenderer, YAMLRenderer,JSONPRenderer)
+    #renderer_classes = (BrowsableAPIRenderer, JSONRenderer, JSONPRenderer, YAMLRenderer)
     queryset = Dokarrs.objects.using('dokarrs').all()
     serializer_class = DokarrsSerializer
-    filter_backends = (filters.DjangoFilterBackend,filters.SearchFilter,)
+    filter_backends = (filters.DjangoFilterBackend, filters.SearchFilter,)
     filter_class = DokarrsFilter
-    search_fields =('genus', 'species', 'common_name', 'county','local','obsvr',
-                    'month','year','class_field','day','remarks','citatin','family',
-                    'obsnum',)
+    search_fields = ('genus', 'species', 'common_name', 'county', 'local', 'obsvr',
+                     'month', 'year', 'class_field', 'day', 'remarks', 'citatin', 'family',
+                     'obsnum', 'number', 'habitat', 'museum',)
