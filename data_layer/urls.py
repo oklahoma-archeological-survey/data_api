40163e45cef82f048791eba4ccf5a2a17a3804c0
Mark Stacy <mbstacy@gmail.com>
2014-07-14 14:15:05 -0500

    Modified urls to include oas_doe

--- data_layer/urls.py
+++ data_layer/urls.py
@@ -5,8 +5,12 @@ admin.autodiscover()
 
 from rest_framework import routers
 from dokarrs_app.views import DokarrsViewSet
-router = routers.DefaultRouter()
-router.register('dokarrs',DokarrsViewSet)
+from oas_doe.views import DoeViewSet
+from views import APIRoot
+
+router = routers.SimpleRouter()
+#router.register('dokarrs', DokarrsViewSet)
+router.register('doe',  DoeViewSet)
 
 urlpatterns = patterns('',
     # Examples:
@@ -15,4 +19,5 @@ urlpatterns = patterns('',
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', APIRoot.as_view()),
 )
